a3788694d8f199f3eae2b8746856111f99379554
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-06 20:05:41 +1000

    added code for user to be defined using sequelize

---
+++ models/User.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  User.associate = function (models) {
+    User.hasMany(models.Post, {
+      onDelete: "cascade",
+    });
+  };
+
+  return User;
+};
