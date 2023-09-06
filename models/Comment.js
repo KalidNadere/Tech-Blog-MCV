44d70b87029cacfbcf5229341f053217b2e4e851
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-06 20:46:34 +1000

    added code to define models/Commet.js, added comments to explain functions of this file

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) { // Exporting function with 2 parameters; sequelize and DataTypes
+  const Comment = sequelize.define("comment", { // Defines sequelize model 'Comment' with attributes specified within object 
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+
+  Comment.associate = function (models) { // defines associations for Comment
+    Comment.belongsTo(models.User, { // establishes relationship between Comment model and User model
+      foreignKey: { // specified to ensure that foreign key referencing User is not null
+        allowNull: false,
+      },
+    });
+    Comment.belongsTo(models.Post, { // Establishes relationship between Comment model and Post model
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+  return Comment; // Exports Comment for use in other parts of the app
+};
