f293cc712d3fff74c67147952081b3493c8f0b73
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-08 19:25:49 +1000

    added code for html.js router to render homepage, dashboard, as well as login, signup and blog post

---
+++ routes/html.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const postController = require("../controllers/postControllers");
+const authController = require("../controllers/authControllers");
+
+// Homepage
+router.get("/", postController.renderHomepage);
+
+// Dashboard (require authentication)
+router.get("/dashboard", authController.isAuthenticated, postController.renderHomepage);
+
+// Login page
+router.get("/login", authController.renderLoginPage);
+
+// Signup page
+router.get("/signup", authController.renderSignupPage);
+
+// Blog post view
+router.get("/post/:id", postController.renderPostView);
+
+module.exports = router;
