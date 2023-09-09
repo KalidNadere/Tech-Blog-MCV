be04f26a31bd92fed28a2c45bb4c18e5995adafa
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-09 19:34:06 +1000

    pushing latest update

--- config/index.js
+++ config/index.js
@@ -1,27 +1,27 @@
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+// const Sequelize = require("sequelize");
+// const env = process.env.NODE_ENV || "development";
+// const config = require(__dirname + "/../config/config.json")[env];
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config
+// );
 
-const db = {};
+// const db = {};
 
-const User = require("../models/User");
-const Post = require("../models/Post");
-const Comment = require("../models/Comment");
+// const User = require("../models/User");
+// const Post = require("../models/Post");
+// const Comment = require("../models/Comment");
 
-db.User = User(sequelize, Sequelize);
-db.Post = Post(sequelize, Sequelize);
-db.Comment = Comment(sequelize, Sequelize);
+// db.User = User(sequelize, Sequelize);
+// db.Post = Post(sequelize, Sequelize);
+// db.Comment = Comment(sequelize, Sequelize);
 
-// Define associations here if needed
+// // Define associations here if needed
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
 
-module.exports = db;
+// module.exports = db;
