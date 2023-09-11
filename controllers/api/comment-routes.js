c7fdf87792e6c419ee0c8514989d8ee2c459908d
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-11 15:50:31 +1000

    created api folder and modified comment-routes.js to create, updated & delete comment

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Create a new comment
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a comment
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedComment = await Comment.update(
+      {
+        ...req.body,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!updatedComment) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const deletedComment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedComment) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(deletedComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
