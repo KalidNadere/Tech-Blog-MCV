3a19107ce270187f79a789123826c314d68f8157
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-11 17:02:34 +1000

    added function to handle logout when clicked, error handling

---
+++ Public/Javascript/logout.js
@@ -0,0 +1,19 @@
+// Function to handle logout when the logout link is clicked
+async function logoutHandler() {
+  // Send a POST request to '/api/logout' route to log user out
+  const response = await fetch('/api/logout', {
+    method: 'POST', // Use the POST method for logging out
+    headers: { 'Content-Type': 'application/json' }, // Set request headers
+  });
+
+  if (response.ok) {
+    // If the response status is OK, redirect to the homepage or another page
+    document.location.replace('/');
+  } else {
+    // If response status is not OK, show an alert indicating logout failure
+    alert('Logout failed. Please try again.');
+  }
+}
+
+// Event listener to the logout link to call the logoutHandler function when the link is clicked
+document.querySelector('#logout-link').addEventListener('click', logoutHandler);
