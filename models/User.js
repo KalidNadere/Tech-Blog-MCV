83edfcbbd818df965f8b3bf338406c1273d9fc81
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-11 16:31:35 +1000

    modified models/user.js to create user model by extending model class, check if password matches user's hashed password

--- models/User.js
+++ models/User.js
@@ -1,26 +1,53 @@
-const { DataTypes } = require("sequelize");
+// Importing necessary modules
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-// Exporting sequelize
-module.exports = (sequelize) => { 
+// Creating User model by extending the Model class
+class User extends Model {
 
-// Defining sequelize model 'User', specifying its attributes and their data types
-  const User = sequelize.define("User", { 
-    username: { 
+  // Method to check if provided password matches the user's hashed password
+  checkPassword(loginPassword) {
+    return bcrypt.compareSync(loginPassword, this.password);
+  }
+}
+
+// Initializing User model with its attributes and data types
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [8], // Minimum character length for the password
+      },
     },
-  });
+  },
+  {
+    hooks: {
+      // Hash the user's password before creating new user
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,    // Passing the sequelize connection
+    freezeTableName: true, // Prevents pluralization of the table name
+    underscored: true,    // Uses underscores in column names
+    modelName: 'user',   // Model name used in queries
+  }
+);
 
-// Defining association between User model and Post model, using hasMany association
-  User.associate = function (models) { 
-    User.hasMany(models.Post, {
-      onDelete: "cascade",
-    });
-  };
-
-  return User; // Exporting the User model, to be used in other parts of the app
-};
+// Export the User model for use in other parts of the application
+module.exports = User;
