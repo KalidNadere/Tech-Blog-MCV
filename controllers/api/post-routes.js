ec1b6e64ae3d9e46ab8654473ad38db4d1050b38
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-09-11 15:52:30 +1000

    modified post-routes.js to Get posts, create, update and delete post using async function

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'user_id', 'post_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username'],
+          },
+        },
+      ],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a post by ID
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!updatedPost[0]) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post updated successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post by ID
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
